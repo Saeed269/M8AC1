28d2e9302ef9cfeb2923e7844630d3f2bdf36c7b
saeed296 <saeed.iqbal@estudiant.fjaverianas.com>
2026-03-30 20:35:45 +0200

    fix: implementar controller deleteProduct

--- controllers/api/deleteProduct.js
+++ controllers/api/deleteProduct.js
@@ -1,1 +1,24 @@
+const client = require('../../db/mongodb-connection');
 
+const deleteProduct = async (req, res) => {
+    const { productID } = req.params;
+    const { ObjectId } = require('mongodb');
+    
+    try {
+        await client.connect();
+        const db = client.db('products');
+        const collection = db.collection('products');
+        
+        const result = await collection.deleteOne({ _id: new ObjectId(productID) });
+        
+        if (result.deletedCount === 0) {
+            return res.status(404).json('Product not found');
+        }
+        
+        res.json('Product deleted successfully');
+    } catch (error) {
+        res.status(500).json('Error deleting product');
+    }
+}
+
+module.exports = deleteProduct;
