ee091d0fdc91d92f1f015efdaa5486cdcc41b5cd
saeed296 <saeed.iqbal@estudiant.fjaverianas.com>
2026-04-01 13:37:40 +0200

    feat: URI MongoDB via variable de entorno

--- config/config.js
+++ config/config.js
@@ -1,4 +1,8 @@
 
 module.exports = {
-  mongoURI: "mongodb+srv://m8user:vuJa6LxdM4Hq6JAH@cluster0.zlbgoxi.mongodb.net/products?retryWrites=true&w=majority"
-};
+    mongoURI: process.env.MONGOURI
+}
+
+// module.exports = {
+//   mongoURI: "mongodb+srv://m8user:vuJa6LxdM4Hq6JAH@cluster0.zlbgoxi.mongodb.net/products?retryWrites=true&w=majority"
+// };
